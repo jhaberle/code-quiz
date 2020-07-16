a2cfa32c7b8b9251c2e7c4f9179e39960719367b
Jake Haberle <jake.haberle@gmail.com>
2020-07-16 06:14:18 -0600

    added questions var to js

---
+++ script.js
@@ -0,0 +1,21 @@
+var codeQuestions = [{
+    question: "Arrays in JavaScript can be used to store _________.",
+    answerList: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
+    answer = 4
+},{
+    question: "The condition in an if / else statement is enclosed within __________.",
+    answerList: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+    answer = 3
+},{
+    question: "Commonly used data types DO NOT include:",
+    answerList: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    answer = 3
+},{
+    question: "Sting values must be enclosed within _________ when being assigned to variables",
+    answerList: ["1. Quotes", "2. Curly Brackets", "3. Commas", "4. Parentheses"],
+    answer = 1
+},{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answerList: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console Log"],
+
+}];
