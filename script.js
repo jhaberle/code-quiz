529d03990707647f45ca0a0519e21bfa11f65ab4
Jake Haberle <jake.haberle@gmail.com>
2020-07-16 23:08:00 -0600

    added global variables and started writting out functions

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
 var codeQuestions = [{
     question: "Arrays in JavaScript can be used to store _________.",
     answerList: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
-    answer = 4
+    
 },{
     question: "The condition in an if / else statement is enclosed within __________.",
     answerList: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
@@ -19,3 +22,24 @@ var codeQuestions = [{
     answerList: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console Log"],
 
 }];
+
+var StartGame = document.querySelector("start-button");
+
+StartGame.addEventListener("click", function(){
+    for(var i = 0; i < codeQuestions.length; i++);
+    if (answer === false) {
+        incorrectAnswers++
+        alert("INCORRECT")
+    } else {
+        correctAnswers++
+        alert("CORRECT")
+    }
+});
+
+
+// function() {
+//     if (answer != 4) {
+//         alert("INCORRECT")
+//     } else {
+//         alert("CORRECT")
+//     }
