a99cfb9cecb4f48a39f24e9f51b2a470b855ccf0
Jake Haberle <jake.haberle@gmail.com>
2020-07-19 16:05:52 -0600

    added in questions on js

--- script.js
+++ script.js
@@ -54,3 +54,58 @@
 
 
 // ----------------------------------------
+
+var question = document.querySelector('#question');
+var choices = arrray.from(document.querySelectorAll('.choice-text'));
+var progressText = document.querySelector('#progressText');
+var scoreText = document.querySelector('#score');
+var progressBarFull = document.querySelector('#progressBarFull');
+
+var currentQuestion = {}
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions = [
+    {
+        question: 'Arrays in JavaScript can be used to store _________.',
+        choice1: 'Numbers and strings',
+        choice2: 'Other arrays',
+        choice3: 'Booleans',
+        choice4: 'All of the Above',
+        answer: 4,
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed within __________.',
+        choice1: 'Quotes',
+        choice2: 'Curly Brackets',
+        choice3: 'Parenthesis',
+        choice4: 'Square Brackets',
+        answer: 3,
+    },
+    {
+        question: 'Commonly used data types DO NOT include:',
+        choice1: 'Strings',
+        choice2: 'Booleans',
+        choice3: 'Alerts',
+        choice4: 'Numbers',
+        answer: 3,
+    },
+    {
+        question: 'Sting values must be enclosed within _________ when being assigned to variables',
+        choice1: 'Quotes',
+        choice2: 'Curly Brackets',
+        choice3: 'Commas',
+        choice4: 'Parenthesis',
+        answer: 1,
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choice1: 'JavaScript',
+        choice2: 'Terminal/Bash',
+        choice3: 'For loops',
+        choice4: 'Console Log',
+        answer: 4,
+    }
+]
