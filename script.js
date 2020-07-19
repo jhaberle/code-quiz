a81d4cb718abd698ba3b28996f5f8f80321d1693
Jake Haberle <jake.haberle@gmail.com>
2020-07-19 13:41:59 -0600

    updated/added html and styles pages

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-var correctAnswers = 0;
-var incorrectAnswers = 0;
+// var correctAnswers = 0;
+// var incorrectAnswers = 0;
+// var StartGame = document.querySelector("#start-game");
 
-var codeQuestions = [{
-    question: "Arrays in JavaScript can be used to store _________.",
-    answerList: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
+
+// var codeQuestions = [{
+//     question: "Arrays in JavaScript can be used to store _________.",
+//     answerList: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
     
-},{
-    question: "The condition in an if / else statement is enclosed within __________.",
-    answerList: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
-    answer = 3
-},{
-    question: "Commonly used data types DO NOT include:",
-    answerList: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-    answer = 3
-},{
-    question: "Sting values must be enclosed within _________ when being assigned to variables",
-    answerList: ["1. Quotes", "2. Curly Brackets", "3. Commas", "4. Parentheses"],
-    answer = 1
-},{
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    answerList: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console Log"],
+// },{
+//     question: "The condition in an if / else statement is enclosed within __________.",
+//     answerList: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+//     answer = 3
+// },{
+//     question: "Commonly used data types DO NOT include:",
+//     answerList: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+//     answer = 3
+// },{
+//     question: "Sting values must be enclosed within _________ when being assigned to variables",
+//     answerList: ["1. Quotes", "2. Curly Brackets", "3. Commas", "4. Parentheses"],
+//     answer = 1
+// },{
+//     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     answerList: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console Log"],
 
-}];
+// }];
 
-var StartGame = document.querySelector("start-button");
+// // --------add event listener not working right now------------------
 
-StartGame.addEventListener("click", function(){
-    for(var i = 0; i < codeQuestions.length; i++);
-    if (answer === false) {
-        incorrectAnswers++
-        alert("INCORRECT")
-    } else {
-        correctAnswers++
-        alert("CORRECT")
-    }
-});
+// StartGame.addEventListener("click", function(){
+//     // newGame();
+//     alert("start button clicked");
+// });
 
+
+
+// function newGame () {
+//     for(var i = 0; i < codeQuestions.length; i++);
+//     if (answer === false) {
+//         incorrectAnswers++
+//         alert("INCORRECT")
+//     } else {
+//         correctAnswers++
+//         alert("CORRECT")
+//     }
+// };
 
 // function() {
 //     if (answer != 4) {
@@ -43,3 +51,6 @@ StartGame.addEventListener("click", function(){
 //     } else {
 //         alert("CORRECT")
 //     }
+
+
+// ----------------------------------------
