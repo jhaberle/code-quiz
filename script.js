0d2208d26fc85e416ed37b8156ff5cd425677ce9
Jake Haberle <jake.haberle@gmail.com>
2020-07-19 21:53:59 -0600

    added timer and some js edits

--- script.js
+++ script.js
@@ -1,60 +1,3 @@
-// var correctAnswers = 0;
-// var incorrectAnswers = 0;
-// var StartGame = document.querySelector("#start-game");
-
-
-// var codeQuestions = [{
-//     question: "Arrays in JavaScript can be used to store _________.",
-//     answerList: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
-    
-// },{
-//     question: "The condition in an if / else statement is enclosed within __________.",
-//     answerList: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
-//     answer = 3
-// },{
-//     question: "Commonly used data types DO NOT include:",
-//     answerList: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-//     answer = 3
-// },{
-//     question: "Sting values must be enclosed within _________ when being assigned to variables",
-//     answerList: ["1. Quotes", "2. Curly Brackets", "3. Commas", "4. Parentheses"],
-//     answer = 1
-// },{
-//     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//     answerList: ["1. JavaScript", "2. Terminal/Bash", "3. For loops", "4. Console Log"],
-
-// }];
-
-// // --------add event listener not working right now------------------
-
-// StartGame.addEventListener("click", function(){
-//     // newGame();
-//     alert("start button clicked");
-// });
-
-
-
-// function newGame () {
-//     for(var i = 0; i < codeQuestions.length; i++);
-//     if (answer === false) {
-//         incorrectAnswers++
-//         alert("INCORRECT")
-//     } else {
-//         correctAnswers++
-//         alert("CORRECT")
-//     }
-// };
-
-// function() {
-//     if (answer != 4) {
-//         alert("INCORRECT")
-//     } else {
-//         alert("CORRECT")
-//     }
-
-
-// ----------------------------------------
-
 var question = document.getElementById("question");
 var choices = Array.from(document.getElementsByClassName("choice-text"));
 var progressText = document.getElementById('progressText');
@@ -127,7 +70,7 @@ getNewQuestion = () => {
         localStorage.setItem("mostRecentScore", score);
 
         //go to the end page
-        return window.location.assign("storage.html");
+        return window.location.assign("highScores.html");
     }
     questionCounter++;
     progressText.innerText = ` Question ${questionCounter}/${MAX_QUESTIONS}`;
@@ -180,3 +123,20 @@ incrementScore = num => {
 };
 
 startGame();
+
+// timer-------------------------
+
+var timeLeft = 30;
+    var elem = document.getElementById('timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
